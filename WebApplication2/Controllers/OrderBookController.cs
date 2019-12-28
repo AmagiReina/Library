a9a40c81b2daa999fff02026993789d331a535ff
НупбаевЖ <@405-04>
2019-12-28 16:11:10 +0600

    added 2 partial views, ajax

--- WebApplication2/Controllers/OrderBookController.cs
+++ WebApplication2/Controllers/OrderBookController.cs
@@ -15,6 +15,10 @@ namespace WebApplication2.Controllers
             using (Model1 db = new Model1())
             {
                 var orderBooks = db.OrderBooks.Include(nameof(db.Users)).Include(nameof(db.Books)).ToList();
+
+                ViewBag.UserList = new SelectList(db.Users.ToList(), "Id", "UsersName");
+                ViewBag.BookList = new SelectList(db.Books.ToList(), "Id", "Title");
+
                 return View(orderBooks);
             }
         }
@@ -44,6 +48,66 @@ namespace WebApplication2.Controllers
                 return View(order);
             }
                
+        }
+
+        public ActionResult GetTop5(int userId)
+        {
+            using (Model1 db = new Model1())
+            {
+                var book = db.OrderBooks.Where(o => o.UserId == userId).Take(5).Select(o => o.BookId);
+
+                /*TO DO Change to Lambda ForEach*/
+                List<Books> bookList = new List<Books>();
+                foreach (int item in book)
+                {
+                    var l = db.Books.Where(b => b.Id == item).FirstOrDefault();
+                    bookList.Add(l);
+                }
+                return new JsonResult
+                {
+                    Data = bookList,
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            using (Model1 db = new Model1())
+            {
+                ViewBag.UserList = new SelectList(db.Users.ToList(), "Id", "UsersName");
+                ViewBag.BookList = new SelectList(db.Books.ToList(), "Id", "Title");
+                return PartialView("Partial/_CreatePartialView");
+            }
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(OrderBook order)
+        {
+            using (Model1 db = new Model1())
+            {
+                ViewBag.UserList = new SelectList(db.Users.ToList(), "Id", "UsersName");
+                ViewBag.BookList = new SelectList(db.Books.ToList(), "Id", "Title");
+                if (Request.IsAjaxRequest())
+                {
+
+                }
+
+                if (ModelState.IsValid)
+                {
+                    order.orderDate = DateTime.Now;
+                    db.OrderBooks.Add(order);
+                    db.SaveChanges();
+                }
+                return null;
+                /*return new JsonResult
+                {
+                    Data = db.OrderBooks.ToList(),
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };*/
+            }
         }
     }
 }
